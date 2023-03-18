17edf3e6f3e704031d2804d0da841115a978fb54
tawaluk <tawaluk@yandex.ru>
2023-03-18 16:51:10 +0300

    осталось только колличетсво постов

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -25,21 +25,35 @@ class PostPagesTests(TestCase):
             group=cls.group,
         )
 
+        cls.ROOT_REVERSE = reverse('posts:index')
+        cls.CRATE_REVERSE = reverse('posts:create')
+        cls.GROUP_REVERSE = reverse(
+            'posts:group_posts',
+            kwargs={'slug': cls.group.slug})
+        cls.PROFILE_REVERSE = reverse(
+            'posts:profile',
+            kwargs={'username': cls.user.username})
+        cls.POST_DETAIL_REVERSE = reverse(
+            'posts:post_detail',
+            kwargs={'post_id': cls.post.id})
+        cls.EDIT_REVERSE = reverse(
+            'posts:edit',
+            kwargs={'post_id': cls.post.id, })
+
     def setUp(self):
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
 
     def check_post_info(self, post):
-        with self.subTest(post=post):
-            self.assertEqual(post.text, self.post.text)
-            self.assertEqual(post.author, self.post.author)
-            self.assertEqual(post.group.id, self.post.group.id)
+        self.assertEqual(post.text, self.post.text)
+        self.assertEqual(post.author, self.post.author)
+        self.assertEqual(post.group.id, self.post.group.id)
 
     def test_forms_show_correct(self):
         """Проверка коректности формы."""
         context = {
-            reverse('posts:create'),
-            reverse('posts:edit', kwargs={'post_id': self.post.id, }),
+            self.CRATE_REVERSE,
+            self.EDIT_REVERSE,
         }
         for reverse_page in context:
             with self.subTest(reverse_page=reverse_page):
@@ -53,34 +67,24 @@ class PostPagesTests(TestCase):
 
     def test_index_page_show_correct_context(self):
         """Шаблон index.html сформирован с правильным контекстом."""
-        response = self.authorized_client.get(reverse('posts:index'))
+        response = self.authorized_client.get(self.ROOT_REVERSE)
         self.check_post_info(response.context['page_obj'][0])
 
     def test_groups_page_show_correct_context(self):
         """Шаблон group_list.html сформирован с правильным контекстом."""
-        response = self.authorized_client.get(
-            reverse(
-                'posts:group_posts',
-                kwargs={'slug': self.group.slug})
-        )
+        response = self.authorized_client.get(self.GROUP_REVERSE)
         self.assertEqual(response.context['group'], self.group)
         self.check_post_info(response.context['page_obj'][0])
 
     def test_profile_page_show_correct_context(self):
         """Шаблон profile.html сформирован с правильным контекстом."""
-        response = self.authorized_client.get(
-            reverse(
-                'posts:profile',
-                kwargs={'username': self.user.username}))
+        response = self.authorized_client.get(self.PROFILE_REVERSE)
         self.assertEqual(response.context['author'], self.user)
         self.check_post_info(response.context['page_obj'][0])
 
     def test_detail_page_show_correct_context(self):
         """Шаблон post_detail.html сформирован с правильным контекстом."""
-        response = self.authorized_client.get(
-            reverse(
-                'posts:post_detail',
-                kwargs={'post_id': self.post.id}))
+        response = self.authorized_client.get(self.POST_DETAIL_REVERSE)
         self.check_post_info(response.context['post'])
 
 
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -93,6 +93,6 @@ LOGIN_REDIRECT_URL = 'posts:index'
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
-NUM_OF_POSTS = 10  # Выборка по N-постов
+NUM_OF_POSTS = 100  # Выборка по N-постов
 
 BATCH_SIZE = 100
